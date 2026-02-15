a3adeda38e44cf32a45bd92603ee3947fa919fa4
Soni <sonirizvi030925@eng.rizvi.edu.in>
2026-02-15 18:44:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Soni Chauhan
+# Date: 11/02/2026
 
 print("--- Extracting Words from Text File ---\n")
+
+with open("story.txt", "r") as file:
+    content = file.read()
+    words = content.split()
+    word_length = int(input("Enter length of Words: "))
+    unique_set = set()
+
+    for word in words:
+        word = word.lower()
+        if len(word) == word_length:
+            unique_set.add(word)
+
+    print(f"\nWords with length {word_length} are: {sorted(unique_set)}")        
 
